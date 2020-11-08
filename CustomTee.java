04e32793704ee7acf4eb0c9877fc20c1de44abc8
Ramakrisnan Ramachanthiran <ramakrishnr@hotmail.com>
2020-11-09 05:39:39 +0800

    Add new files

---
+++ CustomTee.java
@@ -0,0 +1,13 @@
+public class CustomTee extends TeeShirt{
+    //fields
+    private String slogan;
+    
+    //mutator
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+    //accessor
+    public String getSlogan() {
+        return slogan;
+    }
+}
